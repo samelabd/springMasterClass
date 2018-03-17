67d9dc1e70fb6919bf91e74b651c446f54b29e06
kbanach <kbanach@kbanach-h110m-pro.fritz.box>
2018-03-17 14:11:30 +0100

    fix: rename projects

---
+++ spring-mvc-demo-1/src/com/kbanach/springdemo/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.kbanach.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public String doWelcome(Model model) {
+		
+		
+		return "Hello world!";
+	}
+}
